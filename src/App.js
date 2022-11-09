1fbf25f3f6b73ef50db1310264dd2ce6bd9345d4
Priscila Franco <francopri@hotmail.com>
2022-11-09 18:19:52 -0300

    lab - day3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import 'antd/dist/antd.min.css';
 import './App.css';
+import AllFoods from './components/AllFoods';
+// import { Card, Row, Col, Divider, Input, Button } from 'antd';
+
+
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <AllFoods></AllFoods>
+
+
     </div>
   );
 }
