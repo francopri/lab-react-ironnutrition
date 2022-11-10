560a41554edb79417861fa8762dfbf2039019665
Priscila Franco <francopri@hotmail.com>
2022-11-09 23:10:01 -0300

    lab day3 - incompleto

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import 'antd/dist/antd.min.css';
 import './App.css';
-import AllFoods from './components/AllFoods';
-// import { Card, Row, Col, Divider, Input, Button } from 'antd';
-
+import foodsData from './foods.json';
+import FoodBox from './components/FoodBox';
+import AddFoodForm from './components/AddFoodForm';
+import { useState } from 'react';
 
 
 function App() {
 
+  const [foods, setFoods] = useState(foodsData);
+
+  function addNewFood(newFood) {
+
+    const updatedFoods = [...foods, newFood];
+
+    setFoods(updatedFoods);
+
+  }
 
   return (
     <div className="App">
 
-      <AllFoods></AllFoods>
+      <AddFoodForm addFood={addNewFood} />
 
+      <h1>Food List</h1>
+      <hr />
+
+      <div className='food-list'>
+        {
+          foods.map((currentFood) => {
+
+            return (<FoodBox food={currentFood} />);
+          })
+        }
+      </div>
 
     </div>
   );
